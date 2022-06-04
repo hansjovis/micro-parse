9a4ee2a6f8be864d99ca9927a9613cb63e5deaa7
Hans-Christiaan <hans-christiaan@yoast.com>
2022-06-04 19:17:48 +0200

    Added tests and docs for the isEmptyString helper function.

--- src/helpers/isEmptyString.js
+++ src/helpers/isEmptyString.js
@@ -1,5 +1,14 @@
-function isEmptyString( text ) {
-	return text.trim() === "" || /$[\s\n]+^/.test( text );
+/**
+ * Checks if a string is empty or only contains whitespace characters.
+ *
+ * @private
+ *
+ * @param {string} string The string to check.
+ *
+ * @return {boolean} Whether the string is empty or not.
+ */
+function isEmptyString( string ) {
+	return string.trim() === "" || /$[\s\n]+^/.test( string );
 }
 
 module.exports = isEmptyString;
---
+++ tests/helpers/isEmptyString.test.js
@@ -0,0 +1,13 @@
+const isEmptyString = require( "../../src/helpers/isEmptyString" );
+
+describe( "The isEmptyString function", () => {
+	it( "returns true when given an empty string", () => {
+		expect( isEmptyString( "" ) ).toBe( true );
+	} );
+	it( "returns false when given a non-empty string", () => {
+		expect( isEmptyString( "not an empty string" ) ).toBe( false );
+	} );
+	it( "returns true when given a string with only whitespace", () => {
+		expect( isEmptyString( " 	  \n" ) ).toBe( true );
+	} );
+} );
